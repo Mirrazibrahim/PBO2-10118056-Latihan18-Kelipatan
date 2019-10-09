15521977c466267c056d07223233b1c2cf14ad3e
Mirraz Ibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-10-09 16:31:32 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118056/latihan18/kelipatan/PBO210118056Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118056.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ */
+public class PBO210118056Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    double i;    
+    for (i = 3.5; i<=35; i+=3.5){
+            System.out.println(i);
+    }
+    }
+    
+}
